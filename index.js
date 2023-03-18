7bf2e93beea230c2fec1d666fa81cd080e007b65
blxck <qwasi.jk@gmail.com>
2023-03-18 10:22:04 -0600

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const app = require('./lib/server')()
-const port = process.env.PORT || 8426
+const port = process.env.PORT || 8624
 app.listen(port, () => {
   console.log('Listening at http://localhost:' + port)
 })
